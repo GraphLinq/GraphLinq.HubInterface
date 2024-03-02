6436136197a01b7b3dbb931ed28025f4f64be9e3
Triicky <mrtriickyx@gmail.com>
2024-03-02 21:43:26 +0100

    fix: style

--- src/composables/useUniswap.ts
+++ src/composables/useUniswap.ts
@@ -1,22 +1,18 @@
 import { abi as SWAP_ROUTER_ABI } from '@intrinsic-network/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json';
 import { abi as QuoterV2ABI } from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json';
-import { JsonRpcProvider, Wallet, Contract, formatEther, parseEther } from 'ethers';
-import { useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { Contract, formatEther, parseEther } from 'ethers';
 
-const providerUrl = 'https://glq-dataseed.graphlinq.io';
-const privateKey = '';
 const SWAP_ROUTER_ADDRESS = '0x47AB4F709b5C250026C4DA83cde56fc2C81a311c';
 const QUOTER_ADDRESS = '0x287a7beF47684D388fa56BFaB859501f9e515B9D';
 const fee = 10000; // 1% pool fee
 
 const useUniswap = () => {
-  const [provider] = useState<JsonRpcProvider>(new JsonRpcProvider(providerUrl));
-  const [wallet] = useState<Wallet | null>(() => privateKey ? new Wallet(privateKey, provider) : null);
-  const [swapRouter] = useState<Contract | null>(() => wallet ? new Contract(SWAP_ROUTER_ADDRESS, SWAP_ROUTER_ABI, wallet) : null);
-  const [quoter] = useState<Contract | null>(() => wallet ? new Contract(QUOTER_ADDRESS, QuoterV2ABI, wallet) : null);
+  const { provider } = useWeb3React();
+  const swapRouter = new Contract(SWAP_ROUTER_ADDRESS, SWAP_ROUTER_ABI, provider);
+  const quoter = new Contract(QUOTER_ADDRESS, QuoterV2ABI, provider);
 
   const quoteSwap = async (inputToken: string, outputToken: string, amountIn: number): Promise<string | null> => {
-    console.log("quoteSwap", quoter);
     if (!quoter) return null;
 
     try {
@@ -28,7 +24,8 @@ const useUniswap = () => {
           amountIn: amountInFormatted,
           sqrtPriceLimitX96: "0"
       };
-      const amountOut = await quoter.callStatic.quoteExactInputSingle(parameters);
+      console.log(parameters);
+      const amountOut = await quoter.quoteExactInputSingle(parameters);
       return formatEther(amountOut[0]);
     } catch (error) {
       console.error('Failed to quote swap:', error);
