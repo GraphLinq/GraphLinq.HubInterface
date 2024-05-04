5dfab6b8eecd7c8ea8ae908e9abc70c0df54b6ec
Triicky <mrtriickyx@gmail.com>
2024-05-04 14:02:46 +0200

    build: add jsbi alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,34 @@
-import path from 'path'
+import path from "path";
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@components': `${ path.resolve(__dirname, 'src/components') }`,
-      '@assets': `${ path.resolve(__dirname, 'src/assets') }`,
-      '@pages': `${ path.resolve(__dirname, 'src/pages') }`,
-      '@hooks': `${ path.resolve(__dirname, 'src/hooks') }`,
-      '@utils': `${ path.resolve(__dirname, 'src/utils') }`,
-      '@enums': `${ path.resolve(__dirname, 'src/enums') }`,
-      '@models': `${ path.resolve(__dirname, 'src/models') }`,
-      '@context': `${ path.resolve(__dirname, 'src/context') }`,
-      '@constants': `${ path.resolve(__dirname, 'src/constants') }`,
-      '@data': `${ path.resolve(__dirname, 'src/data') }`,
-    }
+      "@components": `${path.resolve(__dirname, "src/components")}`,
+      "@assets": `${path.resolve(__dirname, "src/assets")}`,
+      "@pages": `${path.resolve(__dirname, "src/pages")}`,
+      "@hooks": `${path.resolve(__dirname, "src/hooks")}`,
+      "@utils": `${path.resolve(__dirname, "src/utils")}`,
+      "@enums": `${path.resolve(__dirname, "src/enums")}`,
+      "@models": `${path.resolve(__dirname, "src/models")}`,
+      "@context": `${path.resolve(__dirname, "src/context")}`,
+      "@constants": `${path.resolve(__dirname, "src/constants")}`,
+      "@data": `${path.resolve(__dirname, "src/data")}`,
+      jsbi: path.resolve(
+        __dirname,
+        ".",
+        "node_modules",
+        "jsbi",
+        "dist",
+        "jsbi-cjs.js"
+      ),
+    },
   },
   define: {
-    "global.WebSocket": "globalThis.WebSocket"
+    "global.WebSocket": "globalThis.WebSocket",
   },
-})
+});
